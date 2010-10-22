b2e12953efef2674921470286c1ea81fd06f5480
Kevin Chan <Kevin@kevin-chans-macbook-pro.local>
2010-10-21 19:28:29 -0700

    users controller update

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,14 @@ class User < ActiveRecord::Base
     @password = pwd
     return if pwd.blank?
     create_new_salt
-    self.hashed_password = User.encrypted_password(self.password, self.salt)
+    self.hash_password = User.encrypted_password(self.password, self.salt)
   end
   
   def self.authenticate(name, password)
     user = self.find_by_name(name)
     if user
       expected_password = encrypted_password(password, user.salt)
-      if user.hashed_password != expected_password
+      if user.hash_password != expected_password
         user = nil
       end
     end
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
 private
 
 	def password_non_blank
-		errors.add(:password, "Missing password") if hashed_password.blank?
+		errors.add(:password, "Missing password") if hash_password.blank?
 	end
 	
 	def self.encrypted_password(password, salt)
