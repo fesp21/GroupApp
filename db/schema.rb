57bdc0fda00fb0ee806a4c4d75af98b7d1e0abb1
Kevin Chan <Kevin@kevin-chans-macbook-pro.local>
2010-10-21 18:51:07 -0700

    rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20101022014901) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hash_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
