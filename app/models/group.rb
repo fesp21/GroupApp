e87315dae976bd80978c8a5f7b6909b1e6b8c4e6
Jason Lui <jasonlclei@gmail.com>
2010-11-02 01:39:46 -0700

    commented out stuff in group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,9 @@ class Group < ActiveRecord::Base
   has_one :userlist
   has_many :permissions
   
+  /
   def after_create
     permissions.create( :name => "Admin", :description => "Sudo")
   end
+  /
 end
