cb06afc6087d674d0553c32e6d71a23071992802
Kevin Chan <kevinkchan@berkeley.edu>
2010-11-10 21:36:42 -0800

    random updates

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,13 +3,7 @@ class Group < ActiveRecord::Base
   has_many :permissions
   has_many :uploads, :dependent => :destroy
   has_many :todos, :dependent => :destroy
-  has_many :posts, :dependent => :destroy
+  has_many :posts
   has_many :memberships
   has_many :users, :through => :memberships
-  
-  /
-  def after_create
-    permissions.create( :name => "Admin", :description => "Sudo")
-  end
-  /
 end
