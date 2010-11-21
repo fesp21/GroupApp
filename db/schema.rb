821c79e5364196f36b18d15d9ae38e8e42843b80
Jian Da Chen <jianda1990@gmail.com>
2010-11-20 22:12:27 -0800

    migrated calendar

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101121060326) do
+ActiveRecord::Schema.define(:version => 20101121061120) do
+
+  create_table "calendars", :force => true do |t|
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "chats", :force => true do |t|
     t.datetime "created_at"
