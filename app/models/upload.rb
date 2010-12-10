a53eedd252cacce033093bba7b6a73a5ad1b7bf4
Kevin Chan <Kevin@kevin-chans-macbook-pro.local>
2010-12-09 17:34:18 -0800

    s3

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,10 @@
 class Upload < ActiveRecord::Base
 	belongs_to :group
-	has_attached_file :attachment
 	validates_existence_of :group
+	has_attached_file :attachment,
+  :storage => :s3,
+  :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+  :path => ":attachment/:id/:style/:basename.:extension",
+  :bucket => 'storagebucket'
+  validates_attachment_presence :attachment
 end
