6ae8110a149b9b42b4d0012d47ba101ae8bb3e5b
unknown <anothergz@.(none)>
2010-12-12 19:21:22 -0800

    added newsfeed to user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,20 @@ class User < ActiveRecord::Base
     search_condition = "%" + search + "%"
     find(:all, :conditions => ['username LIKE ?', search_condition])
   end
+  
+  def self.findNewsFeed(num)
+	@user = User.find(:all, :conditions => :id == num)[0]
+	if(@user != nil)
+		@my_groups = @user.groups
+		@all_newsfeeds = []
+		@my_groups.each do |group|
+			group.newsfeeds.reverse.first(20).each do |newsfeed|
+				@all_newsfeeds << newsfeed
+			end
+		end
+		
+	
+		return @all_newsfeeds.sort!{|a,b| a.created_at <=> b.created_at}.reverse.first(20)
+	end  
 end
 
