984f4ea847be7bec66ae59191d6de0575f1d0ec1
Jianda <jianda@Jiandas-MacBook-Pro.local>
2010-12-12 19:03:26 -0800

    profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   has_many :memberships
   has_many  :groups, :through => :memberships
+  has_attached_file :photo,
+  :storage => :s3,
+  :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+  :path => ":photo/:id/:style/:basename.:extension",
+  :bucket => 'storagebucket',
+  :styles => {:thumb=> "100x100#",:small  => "150x150>" }
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/gif', 'image/png', 'image/emp']
+  
   acts_as_authentic
   
   def self.search(search)
